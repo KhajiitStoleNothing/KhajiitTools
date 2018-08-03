01c04fb20e01fc7eb88a4bc5305cd1b2297afef0
root <root@localhost.localdomain>
2018-08-03 11:58:21 +0300

    added scancyphers

---
+++ scancyphers.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Scan available cypher suites with openssl (in case of sslscan failure due to client-side certificate authentication)
+HOST=$1
+PORT=$2
+# OpenSSL requires the port number.
+SERVER="$HOST:$PORT"
+DELAY=1
+ciphers=$(openssl ciphers 'ALL:eNULL' | sed -e 's/:/ /g')
+
+echo Obtaining cipher list from $(openssl version).
+
+for cipher in ${ciphers[@]}
+do
+echo -n Testing $cipher...
+result=$(echo -n | openssl s_client -cipher "$cipher" -connect $SERVER 2>&1)
+if [[ "$result" =~ ":error:" ]] ; then
+  error=$(echo -n $result | cut -d':' -f6)
+  echo NO \($error\)
+else
+  if [[ "$result" =~ "Cipher is ${cipher}" || "$result" =~ "Cipher    :" ]] ; then
+    echo YES
+  else
+    echo UNKNOWN RESPONSE
+    echo $result
+  fi
+fi
+sleep $DELAY
+done
